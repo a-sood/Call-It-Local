caf3abf314dfc327c93a238f79088527f3853464
Anil Sood <Anil Sood@DESKTOP-H25DJ4R>
2018-03-13 13:28:13 -0600

    Add project files.

---
+++ ClientApplicationMVC/Controllers/AuthenticationController.cs
@@ -0,0 +1,103 @@
+﻿using ClientApplicationMVC.Models;
+
+using Messages.NServiceBus.Commands;
+using Messages.DataTypes;
+using Messages.ServiceBusRequest;
+using Messages.ServiceBusRequest.Authentication.Requests;
+
+using System.Web.Mvc;
+using AuthenticationService.Database;
+
+namespace ClientApplicationMVC.Controllers
+{
+    /// <summary>
+    /// This class contains the functions responsible for handling requests routed to *Hostname*/Authentication/*
+    /// </summary>
+    public class AuthenticationController : Controller
+    {
+        /// <summary>
+        /// The default method for this controller
+        /// </summary>
+        /// <returns>The login page</returns>
+        public ActionResult Index()
+        {
+            if (Globals.isLoggedIn() == false)
+            {
+                ViewBag.Message = "Please enter your username and password.";
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.Title = Globals.getUser();
+                return View("LoggedInIndex");
+            }
+        }
+
+        public ActionResult AsIsLogin(string usernameText, string passwordText)
+        {
+            LogInRequest request = new LogInRequest(usernameText, passwordText);
+            ServiceBusResponse response = null;
+
+            ServiceBusConnection connection = ConnectionManager.getConnectionObject(Globals.getUser());
+            if (connection == null)
+            {
+                response = ConnectionManager.sendLogIn(request);
+            }
+            else
+            {
+                response = connection.sendLogIn(request);
+            }
+
+            if (response.result == false)
+            {
+                ViewBag.AsIsResponse = "Login Failed";
+                return View("Index");
+            }
+            else
+            {
+                return View("~/Views/Home/Index.cshtml");
+            }
+        }
+
+        /* END THE USERS SESSION AND RETURN THEM TO THE LOGIN SCREEN */
+        public ActionResult Logout()
+        {
+            Globals.endSession();
+            return Index();
+        }
+
+        public ActionResult CreateAccount()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateAccount(CreateAccount account)
+        {
+            /* Trimming all the white spaces */
+            account.phonenumber = account.phonenumber.Trim();
+            account.email = account.email.Trim();
+            account.password = account.password.Trim();
+            account.username = account.username.Trim();
+            account.address = account.address.Trim();
+
+            /* Removing the - from the phone number */
+            account.phonenumber = account.phonenumber.Replace("-", "");
+
+            AuthenticationDatabase db = AuthenticationDatabase.getInstance();
+            ServiceBusResponse response = db.insertNewUserAccount(account);
+            if(response.result) {
+                ViewBag.userCreationSuccess = "Successfully created the new user. You can log in using your new credentials now.";
+                return View("Index");
+            }
+            else {
+                ViewBag.userCreationFailure = "Could not create the new user. " + response.response;
+                return View();
+            }
+            
+        }
+
+        //This class is incomplete and should be completed by the students in milestone 2
+        //Hint: You will need to make use of the ServiceBusConnection class. See EchoController.cs for an example.
+    }
+}
---
+++ ClientApplicationMVC/Scripts/Authentication/AuthenticationFunctions.js
@@ -0,0 +1,116 @@
+﻿// To be completed by students in milestone 2
+/**
+ * Validates the username and password entered into the login page.
+ * If acceptable, the method returns true, else it returns false.
+ */
+function validateLoginForm() {
+    let regex = /\s/g;
+    let showUsernameError, showPasswordError = false;
+
+    /* EMPTY USERNAME */
+    if (asIsLoginForm.usernameText.value === '') {
+        showUsernameError = true;
+        document.getElementById('usernameError').innerHTML = '*required';
+    }
+
+    /* EMPTY PASSWORD */
+    if (asIsLoginForm.passwordText.value === '') {
+        showPasswordError = true;
+        document.getElementById('passwordError').innerHTML = '*required';
+    }
+
+    /* INVALID USERNAME */
+    if (!regex.test(asIsLoginForm.usernameText)) { // TODO: REGEX DOESN'T WORK
+        showUsernameError = true;
+        document.getElementById('usernameError').innerHTML = '*invalid username';
+    }
+
+    /* ACCEPTED LOGIN INFO */
+    if (!showUsernameError)
+        document.getElementById('usernameError').innerHTML = '';
+    if (!showPasswordError)
+        document.getElementById('passwordError').innerHTML = '';
+    if (!(showUsernameError || showPasswordError))
+        return true;
+
+    return false;
+}
+
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+function runthis() {
+    var response = true;
+
+    var email = createAccountForm.email.value;
+    var atpos = email.indexOf("@");
+    var dotpos = email.lastIndexOf(".");
+    var phoneNum = /^\(?([0-9]{3})\)?[-]?([0-9]{3})[-]?([0-9]{4})$/;
+
+    if (createAccountForm.email.value === "") {
+        document.getElementById("emailError").innerHTML = "Please enter an email";
+        response = false;
+    }
+    else if (!validateEmail(email) || atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= email.length) {
+        document.getElementById("emailError").innerHTML = "Please enter a valid email";
+        response = false;
+    }
+    else {
+        document.getElementById("emailError").innerHTML = "";
+    }
+
+    if (createAccountForm.password.value === "") {
+        document.getElementById("passwordError").innerHTML = "Please enter a password";
+        response = false;
+    }
+    else if (createAccountForm.password.value.length < 8)
+    {
+        document.getElementById("passwordError").innerHTML = "Password must be atleast 8 characters";
+        response = false;
+    }
+    else {
+        document.getElementById("passwordError").innerHTML = "";
+    }
+
+    if (createAccountForm.phone.value === "") {
+        document.getElementById("phoneError").innerHTML = "Please enter a phone number";
+        response = false;
+    }
+    else if (!createAccountForm.phone.value.match(phoneNum)) {
+        document.getElementById("phoneError").innerHTML = "Please enter a valid phone number, format xxx-xxx-xxxx";
+        response = false;
+    }
+    else {
+            
+        document.getElementById("phoneError").innerHTML = "";
+    }
+
+    if (createAccountForm.username.value === "") {
+        document.getElementById("usernameError").innerHTML = "Please enter an username";
+        response = false;
+    }
+    else if (createAccountForm.username.value.match(/[\W_]/))
+    {
+        document.getElementById("usernameError").innerHTML = "You can only use alphanumeric characters for a username";
+        response = false;
+    }
+    else {
+        document.getElementById("usernameError").innerHTML = "";
+    }
+
+    if (createAccountForm.address.value === "") {
+        document.getElementById("addressError").innerHTML = "Please enter an address";
+        response = false;
+    }
+    else if (createAccountForm.address.value.match(/[^a-zA-Z0-9_ ]/)) {
+        document.getElementById("addressError").innerHTML = "You can only use alphanumeric characters for address";
+        response = false;
+    }
+    else {
+        document.getElementById("addressError").innerHTML = "";
+    }
+
+    return response;
+}
