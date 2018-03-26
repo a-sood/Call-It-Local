973a975d4ebc79302e6021432e1ecbe25290cf13
BKDuncan <bailey.duncan@ucalgary.ca>
2018-03-26 12:11:49 -0600

    Create Company Directory Microservice

---
+++ CompanyDirectoryService/Database/CompanyDirectoryServiceDatabase.cs
@@ -0,0 +1,90 @@
+﻿using Messages;
+using Messages.Database;
+using Messages.DataTypes;
+using Messages.NServiceBus.Events;
+using Messages.ServiceBusRequest.Echo.Requests;
+
+using MySql.Data.MySqlClient;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/** TODO: COMPLETE DATABASE SCHEMA **/
+/** TODO: ADD DATABASE MODIFYING FUNCTIONS **/
+
+namespace CompanyDirectoryService.Database
+{
+    /// <summary>
+    /// This portion of the class contains methods and functions
+    /// </summary>
+    public partial class CompanyDirectoryServiceDatabase : AbstractDatabase
+    {
+        /// <summary>
+        /// Private default constructor to enforce the use of the singleton design pattern
+        /// </summary>
+        private CompanyDirectoryServiceDatabase() { }
+
+        /// <summary>
+        /// Gets the singleton instance of the database
+        /// </summary>
+        /// <returns>The singleton instance of the database</returns>
+        public static CompanyDirectoryServiceDatabase getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new CompanyDirectoryServiceDatabase();
+            }
+            return instance;
+        }
+
+    }
+
+    /// <summary>
+    /// This portion of the class contains the properties and variables 
+    /// </summary>
+    public partial class CompanyDirectoryServiceDatabase : AbstractDatabase
+    {
+        /// <summary>
+        /// The name of the database.
+        /// Both of these properties are required in order for both the base class and the
+        /// table definitions below to have access to the variable.
+        /// </summary>
+        private const String dbname = "echoservicedb";
+        public override string databaseName { get; } = dbname;
+
+        /// <summary>
+        /// The singleton instance of the database
+        /// </summary>
+        protected static CompanyDirectoryServiceDatabase instance = null;
+
+        /// <summary>
+        /// This property represents the database schema, and will be used by the base class
+        /// to create and delete the database.
+        /// </summary>
+
+        protected override Table[] tables { get; } =
+        {
+            new Table
+            (
+                dbname,
+                "company",
+                new Column[]
+                {
+                    new Column
+                    (
+                        "id", "INT(64)",
+                        new string[]
+                        {
+                            "NOT NULL",
+                            "UNIQUE",
+                            "AUTO_INCREMENT"
+                        }, true
+                    ),
+                }
+            )
+        };
+    }
+}
---
+++ CompanyDirectoryService/Handlers/CreateAccountEventHandler.cs
@@ -0,0 +1,41 @@
+﻿using CompanyDirectoryService.Database;
+using Messages;
+using Messages.NServiceBus.Events;
+
+using NServiceBus;
+using NServiceBus.Logging;
+
+using System.Threading.Tasks;
+
+namespace CompanyDirectoryService.Handlers
+{
+    /// <summary>
+    /// This is the handler class for the reverse echo. 
+    /// This class is created and its methods called by the NServiceBus framework
+    /// </summary>
+    public class CreateAccountEventHandler : IHandleMessages<AccountCreated>
+    {
+    
+        /// <summary>
+        /// This is a class provided by NServiceBus. Its main purpose is to be use log.Info() instead of Messages.Debug.consoleMsg().
+        /// When log.Info() is called, it will write to the console as well as to a log file managed by NServiceBus
+        /// </summary>
+        /// It is important that all logger member variables be static, because NServiceBus tutorials warn that GetLogger<>()
+        /// is an expensive call, and there is no need to instantiate a new logger every time a handler is created.
+        static ILog log = LogManager.GetLogger<AccountCreated>();
+
+        /// <summary>
+        /// Saves the echo to the database
+        /// This method will be called by the NServiceBus framework when an event of type "AsIsEchoEvent" is published.
+        /// </summary>
+        /// <param name="message">Information about the echo</param>
+        /// <param name="context"></param>
+        /// <returns>Nothing</returns>
+        public Task Handle(AccountCreated message, IMessageHandlerContext context)
+        {
+            //CompanyDirectoryServiceDatabase.getInstance().saveAsIsEcho(message);
+            Debug.consoleMsg("Account Created For " + message.username);
+            return Task.CompletedTask;
+        }
+    }
+}
