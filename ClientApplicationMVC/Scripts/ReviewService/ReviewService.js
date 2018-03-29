beae8f76e2d41ced9fff663480501d14faa5b8e0
Anil Sood <Anil Sood@DESKTOP-H25DJ4R>
2018-03-28 22:53:58 -0600

    Added validation for search bar and review text box

---
+++ ClientApplicationMVC/Scripts/ReviewService/ReviewService.js
@@ -0,0 +1,47 @@
+﻿
+/* This is for search bar validation */
+function validateSearch() {
+    var response = true;
+
+    if (searchForm.searchTextBox.value === "") {
+        document.getElementById("searchError").innerHTML = "Please enter some text to search";
+        var searchDiv = document.getElementById("searchResult");
+        if (searchDiv != null) {
+            document.getElementById("searchResult").innerHTML = "";
+        }
+        response = false;
+    }
+    else if (searchForm.searchTextBox.value.match(/[^\w ]/)) {
+        document.getElementById("searchError").innerHTML = "You can only use alphanumeric characters for a name search";
+        var searchDiv = document.getElementById("searchResult");
+        if (searchDiv != null) {
+            document.getElementById("searchResult").innerHTML = "";
+        }
+        response = false;
+    }
+    else {
+        document.getElementById("searchError").innerHTML = "";
+    }
+
+
+    return response;
+}
+
+function validateReview() {
+    var response = true;
+
+    if (reviewForm.reviewTextBox.value === "") {
+        document.getElementById("reviewError").innerHTML = "Please enter some text for review";
+        response = false;
+    }
+    else if (reviewForm.reviewTextBox.value.match(/[^\w .,_@!:;-]/)) {
+        document.getElementById("reviewError").innerHTML = "You can not use special characters in the review text";
+        response = false;
+    }
+    else {
+        document.getElementById("reviewError").innerHTML = "";
+    }
+
+
+    return response;
+}
